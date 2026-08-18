9a726226c92dec966f9ad9fcc169fc36dc5f96ff
rbrundage5 <rbrundage5@gmail.com>
2026-08-18 10:50:01 -0400

    PR12 export atomic Message editor

--- apps/desktop/src-tauri/src/workspace/behavior_completion/mod.rs
+++ apps/desktop/src-tauri/src/workspace/behavior_completion/mod.rs
@@ -1,6 +1,8 @@
+mod message;
 mod sequence;
 mod state;
 
+pub use message::update_sequence_message_complete;
 pub use sequence::{
     add_combined_fragment_operand, reconnect_sequence_message, update_combined_fragment_operand,
     update_execution_specification, update_sequence_message, update_state_invariant,
