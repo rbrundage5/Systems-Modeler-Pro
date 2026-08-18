4c1e14b4aac726622b40fa7384893d01c50cb932
rbrundage5 <rbrundage5@gmail.com>
2026-08-18 11:28:33 -0400

    Export Submachine State command

--- apps/desktop/src-tauri/src/workspace/behavior_completion/mod.rs
+++ apps/desktop/src-tauri/src/workspace/behavior_completion/mod.rs
@@ -10,5 +10,5 @@ pub use sequence::{
     add_combined_fragment_operand, reconnect_sequence_message, update_combined_fragment_operand,
     update_execution_specification, update_sequence_message, update_state_invariant,
 };
-pub use state::{add_composite_state, update_state_transition};
+pub use state::{add_composite_state, add_submachine_state, update_state_transition};
 pub use transition::add_state_transition_complete;
